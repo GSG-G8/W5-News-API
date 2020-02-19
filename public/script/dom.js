793d7998946ed4424cb3db2fb6e4a15e722ca804
ahmadysalah <a.ahmad.salah@gmail.com>
2020-02-19 15:47:59 +0200

    create dom and make it work #25

--- public/script/dom.js
+++ public/script/dom.js
@@ -1,8 +1,50 @@
+const userInput = document.querySelector('#search-bar');
+const mainNews = document.querySelector('.main-content');
+const searchBtn = document.querySelector('#search-btn');
+const eror = document.querySelector('#eror');
+
+fetching({ value: 'palestine' });
 const creatNews = (data) => {
-  const news = document.createlement('div')
-  new.class = 'news';
-  const headLine = document.createlement('h3').class = 'news-h3';
-  const newsImg = document.createlement('img').class = 'news-img';
-  const newsDetail = document.createlement('p').class = 'news-p'
-  const newsDate = document.createlement('p').class = 'news-date';
-}
+  if (Object.keys(data).length === 0) {
+    eror.textContent = 'There is No data search again';
+  }
+  HideNews();
+  data.forEach((element) => {
+    const news = document.createElement('div');
+    news.className = 'news';
+    const headLine = document.createElement('p');
+    headLine.textContent = element.title;
+    headLine.className = 'news-title';
+    const newsImg = document.createElement('img');
+    newsImg.className = 'news-img';
+    newsImg.src = element.urlToImage;
+    const newsDetail = document.createElement('p');
+    const [first] = element.content.split('.');
+    newsDetail.textContent = first;
+    newsDetail.className = 'news-p';
+    const newsDate = document.createElement('p');
+    newsDate.className = 'news-date';
+    newsDate.textContent = element.publishedAt;
+    mainNews.appendChild(news);
+    news.appendChild(headLine);
+    news.appendChild(newsImg);
+    news.appendChild(newsDetail);
+    news.appendChild(newsDate);
+  });
+};
+
+const HideNews = () => {
+  while (mainNews.firstChild) mainNews.removeChild(mainNews.firstChild);
+};
+
+
+searchBtn.addEventListener('click', () => {
+  const value = userInput.value;
+  if (value === '' || value.startsWith(' ')) {
+    eror.textContent = 'Insert Data and search again';
+  } else {
+    eror.textContent = '';
+    const data = { value: value.trim() };
+    fetching(data);
+  }
+});
