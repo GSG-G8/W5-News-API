1455a89587366fe0dea876b6633371660681ff10
ahmadysalah <a.ahmad.salah@gmail.com>
2020-02-19 13:58:51 +0200

    25- fix problems

---
+++ public/script/dom.js
@@ -0,0 +1,8 @@
+const creatNews = (data) => {
+  const news = document.createlement('div')
+  new.class = 'news';
+  const headLine = document.createlement('h3').class = 'news-h3';
+  const newsImg = document.createlement('img').class = 'news-img';
+  const newsDetail = document.createlement('p').class = 'news-p'
+  const newsDate = document.createlement('p').class = 'news-date';
+}
